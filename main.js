7c6c90899b14bc0ab33a28922c004a316ace14c8
Michael Angelo <yamikamisama@gmail.com>
2015-01-06 14:55:21 -0500

    adding fuctionality to the click for for first box

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 var cNote = document.getElementById('cAudio');
+var dNote = document.getElementById('dAudio');
+var eNote = document.getElementById('eAudio');
+var fNote = document.getElementById('fAudio');
+var gNote = document.getElementById('gAudio');
+var aNote = document.getElementById('aAudio');
+var bNote = document.getElementById('bcAudio');
+
+$('#c').mousedown(function(){
+    cNote.currentTime = 0;
+    cNote.play();
+});
